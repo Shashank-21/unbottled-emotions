34ac212ff34b4623245a3d0ce2e36a961a90d189
Shashank B R <shashankbr21494@gmail.com>
2023-02-24 16:43:30 +0530

    color pink changes

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -8,9 +8,7 @@ function ServiceCard({ service }) {
     <div
       className={`w-4/5 md:w-1/2 xl:w-1/4 flex flex-col p-10 ${cardBgColor} shadow-xl rounded-xl mb-10 items-center`}
     >
-      <h5
-        className={`text-xl md:text-3xl xl:text-2xl font-bold mb-5 ${headingColor}`}
-      >
+      <h5 className={`text-xl md:text-2xl font-bold mb-5 ${headingColor}`}>
         {service.name}
       </h5>
       {service.description && (
